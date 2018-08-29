c41268f47a3320a53be5f12e279b85733d63fcde
Arisht Jain <arisht.jain@xooa.com>
2018-08-29 12:46:18 +0530

    wa

--- quickBooksCC.go
+++ quickBooksCC.go
@@ -65,7 +65,7 @@ func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 // saveNewEvent stores the event on the ledger. For each device
 // it will override the current state with the new one
 func (t *SimpleAsset) saveNewEvent(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	if len(args) != 17 {
+	if len(args) != 3 {
 		return shim.Error("incorrect arguments. Expecting full details")
 	}
 	entity := strings.ToLower(args[0])
@@ -93,7 +93,7 @@ func main() {
 // It retrieve all the devices and their last states for that location.
 func (t *SimpleAsset) getHistoryForEntity(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 
-	if len(args) < 2 {
+	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
@@ -141,7 +141,7 @@ func (t *SimpleAsset) getEntityDetails(stub shim.ChaincodeStubInterface, args []
 	var jsonResp string
 	var err error
 
-	if len(args) < 2 {
+	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
